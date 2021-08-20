e657f525311e41e378ca5cf7dda01643f05def3f
dainlinda <dainlinda@gmail.com>
2021-08-20 14:26:11 +0900

    - Feat: static 폴더 추가, recipe model migrate

--- recipe/models.py
+++ recipe/models.py
@@ -2,24 +2,7 @@ from django.db import models
 # ckeditor
 from ckeditor.fields import RichTextField
 from core.models import TimestampModel
-
-class Recipe(TimestampModel):
-    name = models.CharField(max_length=100, verbose_name="레시피 이름")
-    photo = models.CharField(max_length=100, verbose_name="사진 파일명")
-    how_to_mix = models.TextField(verbose_name="제조법")
-    detail_contents = models.RichTextField(verbose_name="디테일 컨텐츠")
-    youtube_link = models.URLField(max_length=2000, verbose_name="유튜브 url")
-    
-    base = models.ManyToManyField(Base, through = 'RecipeBase')
-    ingredient = models.ManyToManyField(Ingredient, through = 'IngredientBase')
-    alcohol_volume = models.ManyToManyField(AlcoholVolume, through = 'AlcoholVolumeBase')
-    flavor = models.ManyToManyField(Flavor, through = 'FlavorBase')
-    
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        db_table = "recipe"
+from account.models import User
 
 # m2m 테이블들
 class Base(TimestampModel):
@@ -46,34 +29,54 @@ class Flavor(TimestampModel):
     class Meta:
         db_table = "flavor"
 
+class Recipe(TimestampModel):
+    name = models.CharField(max_length=100, verbose_name="레시피 이름")
+    photo = models.CharField(max_length=100, verbose_name="사진 파일명")
+    how_to_mix = models.TextField(verbose_name="제조법")
+    detail_contents = RichTextField(verbose_name="디테일 컨텐츠")
+    youtube_link = models.URLField(max_length=2000, verbose_name="유튜브 url")
+    
+    base = models.ManyToManyField(Base, through = 'RecipeBase')
+    ingredient = models.ManyToManyField(Ingredient, through = 'RecipeIngredient')
+    alcohol_volume = models.ManyToManyField(AlcoholVolume, through = 'RecipeAlcoholVolume')
+    flavor = models.ManyToManyField(Flavor, through = 'RecipeFlavor')
+    
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = "recipe"
+
+
 # 중간 테이블들
-class RecipeBase(models.Model):
+class RecipeBase(TimestampModel):
     recipe  = models.ForeignKey(Recipe, on_delete = models.CASCADE, null=True)
     base = models.ForeignKey(Base, on_delete = models.CASCADE, null=True)
     
     class Meta:
-    db_table = 'recipe_base'
+        db_table = 'recipe_base'
 
-class IngredientBase(models.Model):
+class RecipeIngredient(TimestampModel):
+    recipe  = models.ForeignKey(Recipe, on_delete = models.CASCADE, null=True)
     ingredient  = models.ForeignKey(Ingredient, on_delete = models.CASCADE, null=True)
-    base = models.ForeignKey(Base, on_delete = models.CASCADE, null=True)
     
     class Meta:
-    db_table = 'ingredient_base'
+        db_table = 'recipe_ingredient'
 
-class AlcoholVolumeBase(models.Model):
+class RecipeAlcoholVolume(TimestampModel):
+    recipe  = models.ForeignKey(Recipe, on_delete = models.CASCADE, null=True)
     alcohol_volume  = models.ForeignKey(AlcoholVolume, on_delete = models.CASCADE, null=True)
-    base = models.ForeignKey(Base, on_delete = models.CASCADE, null=True)
     
     class Meta:
-    db_table = 'alcohol_volume_base'
+        db_table = 'recipe_alcohol_volume'
 
-class FlavorBase(models.Model):
+class RecipeFlavor(TimestampModel):
+    recipe  = models.ForeignKey(Recipe, on_delete = models.CASCADE, null=True)
     flavor  = models.ForeignKey(Flavor, on_delete = models.CASCADE, null=True)
-    base = models.ForeignKey(Base, on_delete = models.CASCADE, null=True)
     
     class Meta:
-    db_table = 'flavor_base'
+        db_table = 'recipe_flavor'
+
 
 class Bookmark(TimestampModel):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE, db_column='user_id')
