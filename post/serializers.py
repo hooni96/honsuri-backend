2422cb9bee878364ed3acbe3b5eb58982f2be0ad
expresshighway <03wow22@gmail.com>
2021-08-27 00:32:10 +0900

    feat: comment_set not completed

--- post/serializers.py
+++ post/serializers.py
@@ -11,25 +11,26 @@ class PostImageSerializer(serializers.ModelSerializer):
         model = PostImage
         fields = ("image",)
 
-class PostSerializer(serializers.ModelSerializer):  
-    photos = PostImageSerializer(many=True, read_only=True)
-
-    class Meta:
-        # 'user_id',
-        model = Post
-        fields = ( "id", "photos", "content", "created_at", )
-        read_only_fields = ("id", "created")
-
-    def create(self, validated_data):
-        image_set = self.context['request'].FILES
-        instance = Post.objects.create(**validated_data)
-        for image_data in image_set.getlist("image"):
-            PostImage.objects.create(post=instance, image=image_data)
-        return instance
-
 class CommentSerializer(serializers.ModelSerializer):
   
     class Meta:
         model = Comment
         fields = ("id", "post", "content", "created_at", )
         read_only_fields = ("id", "created")
+
+class PostSerializer(serializers.ModelSerializer): 
+    photos = PostImageSerializer(many=True, read_only=True)
+    comment_set = CommentSerializer(many=True, read_only=True)
+    comment_count = serializers.IntegerField(source='comment_set.count', read_only=True)
+    class Meta:
+        # 'user_id',
+        model = Post
+        fields = ( "id", "photos", "content", "created_at", "comment_set", "comment_count")
+        read_only_fields = ("id", "created", "comment_count")
+        
+    def create(self, validated_data):
+        image_set = self.context['request'].FILES
+        instance = Post.objects.create(**validated_data)
+        for image_data in image_set.getlist("image"):
+            PostImage.objects.create(post=instance, image=image_data)
+        return instance
--- post/views.py
+++ post/views.py
@@ -3,7 +3,7 @@ from django.core import paginator
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import serializers, status
 from rest_framework.permissions import IsAuthenticated
 from django.http import Http404
 from rest_framework import filters
@@ -66,6 +66,8 @@ class PostList(APIView):
         except EmptyPage:
             posts_page = paginator.page(paginator.num_pages)
         Serializer = PostSerializer(posts_page, many=True)
+        print(Serializer.data)
+
 
         return Response(Serializer.data)
 
