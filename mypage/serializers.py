c6d5a0f45ccbfe45b22e7a6a91d7f1652613925a
dainlinda <dainlinda@gmail.com>
2021-08-27 19:21:09 +0900

    - Feat: 비밀번호 수정 기능  제작중

--- mypage/serializers.py
+++ mypage/serializers.py
@@ -26,3 +26,47 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ['email', 'name', 'nickname', 'phone_number', 
         'alcohol_amount', 'favorite_alcohol', 'favorite_food', 'favorite_combination']
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+class PasswordSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(max_length=128, write_only=True)
+    password = serializers.CharField(validators=[validate_password])
+    new_password1 = serializers.CharField(validators=[validate_password])
+    new_password2 = serializers.CharField(validators=[validate_password])
+
+    def validate(self, data):
+        user_id =  self.context['request'].user.pk
+        email = data.get("email")
+        password = data.get("password", None)
+
+        user = authenticate(email=email, password=password)
+
+        if user is None:
+            return {'id': 'None','email':email}
+        try:
+            payload = JWT_PAYLOAD_HANDLER(user)
+            jwt_token = JWT_ENCODE_HANDLER(payload)
+            update_last_login(None, user)
+
+        except User.DoesNotExist:
+            raise serializers.ValidationError(
+                'User with given username and password does not exist'
+            )
+        return {
+            'id':user.id,
+            'token': jwt_token
+        }
+
+    def create(self, validated_data):
+        user = User.objects.create(**validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+    class Meta:
+        model = User
+        fields = ['email', 'password', 'new_password1', 'new_password2']
+
+
+    
--- mypage/views.py
+++ mypage/views.py
@@ -5,6 +5,9 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import *
 
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
+
 class MyFavoriteView(APIView): 
     '''
     내가 북마크한 레시피 api
@@ -27,6 +30,24 @@ class MyPageView(APIView):
     '''
     def get(self, request):
       user_id = request.user.pk
+      queryset = User.objects.filter(id = user_id)
+      serializer = UserSerializer(queryset, many=True) 
+
+      return Response(serializer.data[0])
+
+class PasswordView(APIView): 
+    '''
+    Password 변경 api
+    ---
+    파라미터: request body로 현재 비밀번호, 새 비밀번호, 새 비밀번호(확인) 입력
+    결과: 성공여부 메시지 반환
+    '''
+    @swagger_auto_schema(request_body=EmailFindSerializer)
+    def patch(self, request):
+      user_id = request.user.pk
+      # 로그인 기능 이용해서 있는 비밀번호인지 확인하고
+      serializer = PasswordSerializer(data = request.data)
+      # 회원가입 기능 이용해서 새로운 비밀번호 등록
       queryset = User.objects.filter(id = user_id)
       serializer = UserSerializer(queryset, many=True) 
 
