72df3095a8029b7df70b39acf8f2e09e5399c069
JihunMin <minzihun@gmail.com>
2021-08-28 12:03:42 +0900

    Docs: comment 수정

--- account/views.py
+++ account/views.py
@@ -31,7 +31,7 @@ class RegisterView(CreateAPIView):
         serializer = UserCreateSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
