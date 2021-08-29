783c09f73324edd3589694f8113ddbf728fad72c
dainlinda <dainlinda@gmail.com>
2021-08-30 01:05:25 +0900

    - newrelic import 제거

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -1,9 +1,9 @@
-import newrelic.agent
+# import newrelic.agent
 import sys
 import os
 from django.core.wsgi import get_wsgi_application
 
-newrelic.agent.initialize('newrelic.ini') # newrelic.ini 파일의 절대경로
+# newrelic.agent.initialize('newrelic.ini') # newrelic.ini 파일의 절대경로
 sys.path.append('/home/honsuri/myvenv/lib/python3.8/site-packages')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
@@ -11,4 +11,4 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 application = get_wsgi_application()
 
 # newrelic
-application = newrelic.agent.wsgi_application()(application) 
+# application = newrelic.agent.wsgi_application()(application) 
