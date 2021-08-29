8271b8a4b4d16642fd78aec738d154530bf92b74
JihunMin <minzihun@gmail.com>
2021-08-29 23:12:05 +0900

    Chore: 배포

--- mysite/settings.py
+++ mysite/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.environ.get('secret')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['honsuri-backend.herokuapp.com']
 
 
 # Application definition
